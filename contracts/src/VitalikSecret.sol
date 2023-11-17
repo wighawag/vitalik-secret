13291e97a5a63a0581dfa927566616208843f318
wighawag <wighawag@gmail.com>
2023-11-17 23:25:00 +0000

    solidity

--- contracts/src/VitalikSecret.sol
+++ contracts/src/VitalikSecret.sol
@@ -7,9 +7,61 @@ import "solidity-kit/solc_0.8/ERC721/interfaces/IERC721Metadata.sol";
 
 /// @notice a puzzle
 contract VitalikSecret is BasicERC721, IERC721Metadata, Proxied {
+    enum Move {
+        RIGHT,
+        DOWN,
+        LEFT,
+        UP
+    }
+
+    uint256 public constant SIZE = 4;
+    bytes public constant SOLUTION = "0x123456789ABCDEF0";
+
+    // TODO immutable but constructor match a pre-agreed block hash
+    bytes public constant RANDOM = "0x213456789ABCDFE0";
+    uint256 immutable INITIAL_POSITION = 15;
+
     // TODO commit first to prevent front-running
-    function proposeSolution(bytes memory moves) external {
-        // TODO check move to match result
+    // function proposeSolution(bytes memory moves) external {
+    function proposeSolution(Move[] calldata moves) external {
+        bytes memory state = RANDOM;
+        uint256 position = INITIAL_POSITION;
+
+        for (uint256 i = 0; i < moves.length; i++) {
+            bool valid;
+            (valid, position) = step(state, position, moves[i]);
+            require(valid, "INVALID_MOVE");
+        }
+    }
+
+    function step(
+        bytes memory currentState,
+        uint256 position,
+        Move move
+    ) internal returns (bool valid, uint256 newPosition) {
+        uint256 x = position % SIZE;
+        uint256 y = position / SIZE;
+        if (move == Move.RIGHT) {
+            if (x < SIZE - 1) {
+                valid = true;
+                // newState =
+            }
+        } else if (move == Move.DOWN) {
+            if (y < SIZE - 1) {
+                valid = true;
+                // newState =
+            }
+        } else if (move == Move.LEFT) {
+            if (x > 0) {
+                valid = true;
+                // newState =
+            }
+        } else if (move == Move.UP) {
+            if (y > 0) {
+                valid = true;
+                // newState =
+            }
+        }
     }
 
     function tokenURI(uint256 tokenID) external view returns (string memory) {
@@ -21,6 +73,17 @@ contract VitalikSecret is BasicERC721, IERC721Metadata, Proxied {
     }
 
     function symbol() external pure returns (string memory) {
-        return "VTKS";
+        return "VTS";
+    }
+
+    // ---------------------------------------------------------------------------
+    function _at(bytes memory data, uint256 pos) internal returns (uint256 v) {
+        assembly {
+            let b := mload(add(add(data, 32), div(pos, 2)))
+            v := and(b, 0xF)
+            if eq(mod(pos, 2), 1) {
+                v := shr(4, b)
+            }
+        }
     }
 }
